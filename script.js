e0227e9caf406e6005898eab5df0850a32c5b8f6
Saikat Guha <sktguha@gmail.com>
2018-09-21 21:19:41 +0530

    add padding

--- script.js
+++ script.js
@@ -9,11 +9,108 @@ document.oncontextmenu = function(e){
 //     if(e.target.)
     if(!$(e.target).parents(pc)[0]) return;
     var dv = document.createElement("div");
-    dv.innerText = "Reply"
+    $(dv).text("Reply");
+    dv.style = "position:absolute;z-index:10000000;" 
+     + "padding: 4px 10px 4px 10px;background:black;color:white";
     dv.style.left = e.clientX + "px";
     dv.style.top = e.clientY + "px";
-    dv.style.position = "absolute";
-    dv.style.zIndex = "10000000";
+    
+    dv.dataset.srcElem = e.target.innerText;
+    document.body.appendChild(dv);
+    dv.tabIndex = -1;
+    dv.focus();
+    dv.onblur = function(){
+        dv.parentElement.removeChild(dv);
+    }
+    dv.onclick = function(){
+        var te = $(e.target).parents('._4tdt').parents(".fbNubFlyoutInner").find(".fbNubFlyoutFooter")[0];
+        var dv = $(te).find("._18yz")[0];
+        
+        var repDv = document.createElement("div");
+        repDv.innerText = e.target.innerText;
+        repDv.dataset.replyContent = e.target.innerText;
+        var elem = e.target;
+        do{
+          elem = elem.parentElement;
+        } while ( elem && elem.className.indexOf("fantaTabMain-user") === -1);
+
+        var cls = Array.from(elem.classList).find(function(cls){
+          return cls.indexOf("fantaTabMain-user:")!==-1;
+        });
+        var userId = cls.split("fantaTabMain-user:")[1]*1;
+        
+        var qid = "replycontentcustom" + userId;
+        repDv.id = qid;
+
+        var cross = document.createElement("cross");
+        cross.innerText = "x";
+        cross.onclick = function(){
+            repDv.parentElement.removeChild(repDv);
+        }
+        
+        repDv.appendChild(cross);
+        dv.prepend(repDv);
+    }
+    return false;
+}
+
+
+registerHook();
+//need to verify if using chrome.tabs.onUpdate.addListener approach is better than this
+//In future changing the requestBody data in a  "post" request using the webrequest api can be used if support is added ,
+// can intercept the network call to send.php
+// https://bugs.chromium.org/p/chromium/issues/detail?id=91191 . (support for this already exists on firefox)
+function intercept(body){
+    if(body && body.indexOf("action_type=ma-type%3Auser-generated-message") !== -1){
+        var paramsArr = body.split("&");
+        var userId = paramsArr.filter(function(part){
+           return part.startsWith("other_user_fbid=");
+        })[0];
+        userId = userId.split("other_user_fbid=")[1]*1;
+        var qid = "replycontentcustom" + userId;
+        var replyDiv = document.getElementById(qid);
+        if(!replyDiv) return body;
+
+        var mbd = paramsArr.findIndex(function(part){
+           return part.startsWith("body=");
+        });
+        if(mbd === -1){
+            return body;
+        }
+        var mparam = paramsArr[mbd];
+        var msg = mparam.split("body=")[1];
+        msg += encodeURIComponent("replied to : " + replyDiv.dataset.replyContent);
+        paramsArr[mbd] = "body="+msg;
+        body = paramsArr.join("&");
+        replyDiv.parentElement.removeChild(replyDiv);
+    }
+    return body;
+}
+function registerHook(){
+(function(send) {
+    XMLHttpRequest.prototype.send = function() {
+        arguments[0] = intercept(arguments[0]);        
+		send.apply(this, arguments);
+    };
+})(XMLHttpRequest.prototype.send);
+}
+var sc = document.createElement("script");
+sc.src = "https://code.jquery.com/jquery-3.3.1.js";
+document.body.appendChild(sc);
+
+document.oncontextmenu = function(e){
+    var pc = "._5yl5";
+    
+//     if(e.target.tagName !== "SPAN") return;
+//     if(e.target.)
+    if(!$(e.target).parents(pc)[0]) return;
+    var dv = document.createElement("div");
+    $(dv).text("Reply");
+    dv.style = "position:absolute;z-index:10000000;" 
+     + "padding: 4px 10px 4px 10px;background:black;color:white";
+    dv.style.left = e.clientX + "px";
+    dv.style.top = e.clientY + "px";
+    
     dv.dataset.srcElem = e.target.innerText;
     document.body.appendChild(dv);
     dv.tabIndex = -1;
